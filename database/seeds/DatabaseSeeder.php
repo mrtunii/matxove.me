78d2776f72df94b77dc5de49da53d24609e83ff9
mrtunii <mrtunii@outlook.com>
2017-01-29 12:51:40 +0400

    updated table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserTableSeeder::class);
+      //  $this->call(UserTableSeeder::class);
 
-         $this->call(CategorySeeder::class);
+        $this->call(CategorySeeder::class);
 
         $this->call(ProductTableSeeder::class);
 
